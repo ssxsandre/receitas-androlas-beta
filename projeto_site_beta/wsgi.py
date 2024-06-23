d8286f781d0bd9e2d8d7c99a8dcf53e4e6bcaf04
androlas <152226218+ssxsandre@users.noreply.github.com>
2024-06-23 13:24:02 -0300

    Atualizar o wsgi.py

--- projeto_site_beta/wsgi.py
+++ projeto_site_beta/wsgi.py
@@ -1,21 +1,6 @@
-"""
-WSGI config for projeto_site_beta project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-import sys
+from django.core.wsgi import get_wsgi_application
 
-# Adiciona o diretório que contém o projeto ao sys.path
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-# Define as configurações do Django para o projeto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_site_beta.settings')
 
-# Obtém a aplicação WSGI padrão do Django
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
